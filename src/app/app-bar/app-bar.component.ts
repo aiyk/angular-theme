6559d646d1c0982819f23c68bb78ab22a40e8b3d
aiyk <aiyk.ekwe@gmail.com>
2018-10-24 11:51:32 +0100

    minor updates

--- src/app/app-bar/app-bar.component.ts
+++ src/app/app-bar/app-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-app-bar',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppBarComponent implements OnInit {
 
+  @Output() toggleMenu = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleMenu_click(){
+    this.toggleMenu.emit(true);
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  menuState = true;
+  
+  toggleMenu_click($event){
+    this.menuState = !this.menuState
+  }
 }
