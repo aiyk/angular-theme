d174fd829ab52bf9bb2fd812e898cd3edc0b167e
aiyk <aiyk.ekwe@gmail.com>
2018-11-01 16:21:30 +0100

    form inputs

--- src/app/design-system-documentation/form-elements/form-elements.component.ts
+++ src/app/design-system-documentation/form-elements/form-elements.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormElementsComponent implements OnInit {
 
+  inputfields = `
+  <input type="text" placeholder="Enter some text">
+
+  <input type="text" placeholder="Enter some text" disabled>
+
+  <input type="text" value="Read only value" readonly>
+
+  <input class="input-invalid" type="text" placeholder="Enter some text">
+
+  <input class="input-valid" type="text" placeholder="Enter some text">
+  
+  <div class="search-wrap">
+    <input type="search" placeholder="Type your search">
+  </div>
+  `;
+
   constructor() { }
 
   ngOnInit() {
