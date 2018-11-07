e7ed8819f39580d61e499a088dfe62f33a50f604
aiyk <aiyk.ekwe@gmail.com>
2018-11-07 13:44:28 +0100

    system alerts and notifications

--- src/app/design-system-documentation/alert-def/alert-def.component.ts
+++ src/app/design-system-documentation/alert-def/alert-def.component.ts
@@ -23,6 +23,26 @@ export class AlertDefComponent implements OnInit {
   successLite_ists = false;
   successLite_iscss = false;
 
+  warningLite_ishtml = true;
+  warningLite_ists = false;
+  warningLite_iscss = false;
+
+  err_ishtml = true;
+  err_ists = false;
+  err_iscss = false;
+
+  info_ishtml = true;
+  info_ists = false;
+  info_iscss = false;
+
+  success_ishtml = true;
+  success_ists = false;
+  success_iscss = false;
+
+  warning_ishtml = true;
+  warning_ists = false;
+  warning_iscss = false;
+
   input_snippet = `
   <div class="form-group">
     <div class="notification input-error">You have a big buggy bug here!!!</div>
@@ -39,6 +59,26 @@ export class AlertDefComponent implements OnInit {
 
   notification_successLite_snippet = `
   <div class="notification default-alert-success">Your alert message goes here</div>
+  `;
+
+  notification_warningLite_snippet = `
+  <div class="notification default-alert-warning">Your alert message goes here</div>
+  `;
+
+  notification_err_snippet = `
+  <div class="notification dismisable-alert-err">Your alert message goes here</div>
+  `;
+
+  notification_info_snippet = `
+  <div class="notification dismisable-alert-info">Your alert message goes here</div>
+  `;
+
+  notification_success_snippet = `
+  <div class="notification dismisable-alert-success">Your alert message goes here</div>
+  `;
+
+  notification_warning_snippet = `
+  <div class="notification dismisable-alert-warning">Your alert message goes here</div>
   `;
 
   constructor() { }
@@ -63,6 +103,26 @@ export class AlertDefComponent implements OnInit {
       this.successLite_ishtml = true;
       this.successLite_ists = false;
       this.successLite_iscss = false;
+    } else if(target==='notification_warning_lite'){
+      this.warningLite_ishtml = true;
+      this.warningLite_ists = false;
+      this.warningLite_iscss = false;
+    } else if(target==='notification_err'){
+      this.err_ishtml = true;
+      this.err_ists = false;
+      this.err_iscss = false;
+    } else if(target==='notification_info'){
+      this.info_ishtml = true;
+      this.info_ists = false;
+      this.info_iscss = false;
+    } else if(target==='notification_success'){
+      this.success_ishtml = true;
+      this.success_ists = false;
+      this.success_iscss = false;
+    } else if(target==='notification_warning'){
+      this.warning_ishtml = true;
+      this.warning_ists = false;
+      this.warning_iscss = false;
     }
   }
   csstab(target){
@@ -82,6 +142,26 @@ export class AlertDefComponent implements OnInit {
       this.successLite_ishtml = false;
       this.successLite_iscss = true;
       this.successLite_ists = false;
+    } else if(target==='notification_warning_lite'){
+      this.warningLite_ishtml = false;
+      this.warningLite_iscss = true;
+      this.warningLite_ists = false;
+    } else if(target==='notification_err'){
+      this.err_ishtml = false;
+      this.err_iscss = true;
+      this.err_ists = false;
+    } else if(target==='notification_info'){
+      this.info_ishtml = false;
+      this.info_iscss = true;
+      this.info_ists = false;
+    } else if(target==='notification_success'){
+      this.success_ishtml = false;
+      this.success_iscss = true;
+      this.success_ists = false;
+    } else if(target==='notification_warning'){
+      this.warning_ishtml = false;
+      this.warning_iscss = true;
+      this.warning_ists = false;
     }
   }
   tstab(target){
@@ -101,7 +181,27 @@ export class AlertDefComponent implements OnInit {
       this.successLite_ishtml = false;
       this.successLite_iscss = false;
       this.successLite_ists = true;
+    } else if(target==='notification_warning_lite'){
+      this.warningLite_ishtml = false;
+      this.warningLite_iscss = false;
+      this.warningLite_ists = true;
+    } else if(target==='notification_err'){
+      this.err_ishtml = false;
+      this.err_iscss = false;
+      this.err_ists = true;
+    } else if(target==='notification_info'){
+      this.info_ishtml = false;
+      this.info_iscss = false;
+      this.info_ists = true;
+    } else if(target==='notification_success'){
+      this.success_ishtml = false;
+      this.success_iscss = false;
+      this.success_ists = true;
+    } else if(target==='notification_warning'){
+      this.warning_ishtml = false;
+      this.warning_iscss = false;
+      this.warning_ists = true;
     }
-  }
+  } 
 
 }
