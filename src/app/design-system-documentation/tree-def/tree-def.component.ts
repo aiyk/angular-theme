8cfed563c9d2db1de9f3c759c0d70d2ee30975e1
aiyk <aiyk.ekwe@gmail.com>
2018-11-13 09:19:24 +0100

    tree list

--- src/app/design-system-documentation/tree-def/tree-def.component.ts
+++ src/app/design-system-documentation/tree-def/tree-def.component.ts
@@ -37,7 +37,57 @@ export class TreeDefComponent implements OnInit {
     }
   ];
 
-  tree_snippet = ``;
+  tree_snippet_html = `
+  <div class="x2-item">
+    <div *ngFor = "let item of list; let i = index" class="tree-wrap">
+      <div class="tree-item">
+        <div (click)="treeItem_toggle(i)" [class.i-open] = item_to_show(i) [class.i-close] = !item_to_show(i) class="i-pointer"></div> 
+        <div class="i-folder"></div>
+        <div class="tree-label">{{item.label}} ({{item.count}})</div>
+      </div>
+      <div *ngIf="item_to_show(i)" class="tree-content">
+        <legend><strong>{{item.label}}</strong> is the base upon which the content seats</legend>
+        <button class="btn-blue btn-plus-left btn-x2">{{item.label}}</button>
+      </div>
+    </div>
+  </div>
+  `;
+  tree_snippet_ts = `
+  treeState = false;
+  clickItemIndex: number;
+
+  list: any[] = [
+    {
+      "label": "Item A",
+      "count": "5",
+    },
+    {
+      "label": "Item B",
+      "count": "6",
+    },
+    {
+      "label": "Item C",
+      "count": "7",
+    },
+    {
+      "label": "Item D",
+      "count": "2",
+    },
+    {
+      "label": "Item E",
+      "count": "15",
+    }
+  ]; 
+
+  item_to_show(i) {
+    if(this.clickItemIndex === i && this.treeState == true) return true;
+    if(this.clickItemIndex === i && this.treeState == false) return false;
+  }
+  treeItem_toggle(itemIndex) {
+    this.clickItemIndex = itemIndex;
+    this.treeState = !this.treeState;
+  }
+  `;
 
   constructor() { }
 
